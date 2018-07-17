0fe27e6cefe2b5c9db4d96a8605272086732578e
bcarney <brett.carney1273@gmail.com>
2018-07-17 09:30:35 -0500

    Styling and Javascript

---
+++ js/win-controls.js
@@ -0,0 +1,29 @@
+(function () {
+
+    const electron = require('electron'); 
+    const {app, BrowserWindow} = electron
+    
+   function init() { 
+        document.getElementById("min-btn").addEventListener("click", function (e) {
+             var window = BrowserWindow.getFocusedWindow();
+             window.minimize(); 
+        });
+
+        document.getElementById("max-btn").addEventListener("click", function (e) {
+             var window = BrowserWindow.getFocusedWindow(); 
+             window.maximize(); 
+        });
+
+        document.getElementById("close-btn").addEventListener("click", function (e) {
+             var window = BrowserWindow.getFocusedWindow();
+             window.close();
+        }); 
+   }; 
+
+   document.onreadystatechange = function () {
+        if (document.readyState == "complete") {
+             init(); 
+        }
+   };
+
+})();
