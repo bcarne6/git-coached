f22abfa7b67aa30142a018145d2be710a359dc26
bcarney <brett.carney1273@gmail.com>
2018-07-20 13:35:33 -0500

    Search functionality

--- js/search-replay.js
+++ js/search-replay.js
@@ -1,14 +1,21 @@
 const fs = require("fs");
 
-function searchBtnChange() {
-    var fileArray = fs.readdir(".", (err, files) => {
+function searchBtnChange(val) {
+    var searchString = $(val).val();
+
+    var fileArray = fs.readdir("testdata", (err, files) => {
         document.getElementById("searchresultstarget").innerHTML = "";
         'use strict';
         //if an error is thrown when reading the directory, we throw it. Otherwise we continue
         if (err) throw  err;
         //the files parameter is an array of the files and folders in the path we passed. So we loop through the array, printing each file and folder
         for (let file of files) {
-            document.getElementById("searchresultstarget").innerHTML += "<button class=\"btn btn-info file-spacing\" onclick=\"toggleFileClass(this)\">" + file + "</button><br />";
+            var filepath = "testdata/" + file;
+            var text = fs.readFileSync(filepath);
+
+            if(text.indexOf(searchString) >= 0){
+                document.getElementById("searchresultstarget").innerHTML += "<button class=\"btn btn-info file-spacing\" onclick=\"toggleFileClass(this)\">" + file + "</button><br />";
+            }            
         }
     });    
 }
