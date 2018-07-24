c594f3e1df42b7f59f59669a4a4ebb3c1206af2b
bcarney <brett.carney1273@gmail.com>
2018-07-24 16:32:47 -0500

    Better Set Directory Fuctionality

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,26 @@
 function setDirectory() {
-    let userDefinedDirectory = $("#setdir").val();
-    let configFile = JSON.parse(fs.readFileSync("./config.json").toString());
-    configFile.filePath = userDefinedDirectory;
-    fs.writeFile("./config.json", configFile, "utf8");
+    let editJsonFile = require("edit-json-file");
+    let config = editJsonFile("./config.json");
+    let pathObject = (<HTMLInputElement>document.getElementById("setdir")).files[0];
+    let replayPath = pathObject.path + "\\";
+    config.set("filePath", replayPath);
+    config.save();
+
+    if($("settingsstatusmsg").hasClass("no-changes")){
+        $("settingsstatusmsg").removeClass("no-changes").addClass("saved-changes");
+    }    
+    else if($("settingsstatusmsg").hasClass("pending-changes")){
+        $("settingsstatusmsg").removeClass("pending-changes").addClass("saved-changes")
+    }
 }
+
+$("#setdir").on("click", function() {
+    if($("#settingsstatusmsg").hasClass("no-changes")){
+        $("settingsstatusmsg").removeClass("no-changes");
+        $("settingsstatusmsg").addClass("pending-changes");
+    }
+    else if($("#settingsstatusmsg").hasClass("saved-changes")){
+        $("settingsstatusmsg").removeClass("saved-changes");
+        $("settingsstatusmsg").addClass("pending-changes");
+    }
+});
